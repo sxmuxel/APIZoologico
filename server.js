97ca0f5830f9ef8b2bac231a0f60ca9329cfa130
sam <sxm@example.com>
2026-04-08 14:10:51 -0500

    Se agrega JWT y bcrypt al proyecto

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const authRoutes = require("./routes/authentication");
 
 const app = express();
 
@@ -16,6 +17,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 // Rutas
 app.use("/api/animals", require("./routes/animalRoutes"));
+app.use("/api", authRoutes);
 
 // Ruta base
 app.get("/", (req, res) => {
